94ec803f3117da2247491ff26217138018c58851
Pedro <pedren83@gmail.com>
2024-09-25 10:46:54 -0300

    Se sube ejercicio 1 del proyecto dos modificado

--- firmware/projects/guia2_ej1/main/guia2_ej1.c
+++ firmware/projects/guia2_ej1/main/guia2_ej1.c
@@ -38,102 +38,114 @@
 #define CONFIG_BLINK_PERIOD_TECLAS 200
 #define CONFIG_BLINK_PERIOD_MEDICION 1000
 /*==================[internal data definition]===============================*/
-bool hold=false;
-bool medir=true;
-uint16_t valor_medicion=0;
+bool hold = false;
+bool medir = true;
+uint16_t valor_medicion = 0;
 /*==================[internal functions declaration]=========================*/
-void Controlar_Leds(void){
+void Controlar_Leds(void)
+{
 
-if(valor_medicion<10){
-	LedOff(LED_1);
-	LedOff(LED_2);
-	LedOff(LED_3);
-}
-else{
-	if(valor_medicion>10 && valor_medicion<20){
-		LedOn(LED_1);
+	if (valor_medicion < 10)
+	{
+		LedOff(LED_1);
 		LedOff(LED_2);
 		LedOff(LED_3);
 	}
-else{
-if(valor_medicion>20 && valor_medicion<30){
-		LedOn(LED_1);
-		LedOn(LED_2);
-		LedOff(LED_3);
-	}
-	else{
-	if(valor_medicion>30){
-		LedOn(LED_1);
-		LedOn(LED_2);
-		LedOn(LED_3);
-	}
-}
-}
-}
-
-}
-
-static void medicion (void *pvParameter){
-
-	while(true){
-		if(medir==true){
-			valor_medicion=HcSr04ReadDistanceInCentimeters();
+	else
+	{
+		if (valor_medicion > 10 && valor_medicion < 20)
+		{
+			LedOn(LED_1);
+			LedOff(LED_2);
+			LedOff(LED_3);
 		}
- 			vTaskDelay(CONFIG_BLINK_PERIOD_MEDICION / portTICK_PERIOD_MS);
+		else
+		{
+			if (valor_medicion > 20 && valor_medicion < 30)
+			{
+				LedOn(LED_1);
+				LedOn(LED_2);
+				LedOff(LED_3);
+			}
+			else
+			{
+				if (valor_medicion > 30)
+				{
+					LedOn(LED_1);
+					LedOn(LED_2);
+					LedOn(LED_3);
+				}
+			}
+		}
 	}
-	
-}
-static void mostrar (void *pvParameter){
-
-while(true){
-
-	if(medir&&hold){
-		Controlar_Leds();
-	}
-	else{
-		LcdItsE0803Write(valor_medicion);
-		Controlar_Leds();
-	}
-	if(medir==false){
-		LcdItsE0803Off();
-		LedOff(LED_1);
-	LedOff(LED_2);
-	LedOff(LED_3);
-	}
-
- vTaskDelay(CONFIG_BLINK_PERIOD_MEDICION / portTICK_PERIOD_MS);
 }
 
+static void medicion(void *pvParameter)
+{
+
+	while (true)
+	{
+		if (medir == true)
+		{
+			valor_medicion = HcSr04ReadDistanceInCentimeters();
+		}
+		vTaskDelay(CONFIG_BLINK_PERIOD_MEDICION / portTICK_PERIOD_MS);
+	}
+}
+static void mostrar(void *pvParameter)
+{
+
+	while (true)
+	{
+		if (medir)
+		{
+			Controlar_Leds();
+			if (!hold)
+			{
+				LcdItsE0803Write(valor_medicion);
+			}
+		}
+		else
+		{
+			LcdItsE0803Off();
+			LedOff(LED_1);
+			LedOff(LED_2);
+			LedOff(LED_3);
+		}
+		vTaskDelay(CONFIG_BLINK_PERIOD_MEDICION / portTICK_PERIOD_MS);
+	}
 }
 
-
-static void teclas (void *pvParameter){
+static void teclas(void *pvParameter)
+{
 	uint8_t teclas;
-    while(true)    {
-    	teclas  = SwitchesRead(); 
-    	switch(teclas){
-    		case SWITCH_1:
-			medir=!medir;
+	while (true)
+	{
+		teclas = SwitchesRead();
+		switch (teclas)
+		{
+		case SWITCH_1:
+			medir = !medir;
 
-    		break;
-    		case SWITCH_2:
-    		hold=!hold;
-    		break;
-    	}
-	    vTaskDelay(CONFIG_BLINK_PERIOD_TECLAS / portTICK_PERIOD_MS);
+			break;
+		case SWITCH_2:
+			hold = !hold;
+			break;
+		}
+		vTaskDelay(CONFIG_BLINK_PERIOD_TECLAS / portTICK_PERIOD_MS);
 	}
 }
 /*==================[external functions definition]==========================*/
-void app_main(void){
-//Inicializo LCD, Switchs y leds
-LcdItsE0803Init();
-LedsInit();
-HcSr04Init(GPIO_3,GPIO_2);
-SwitchesInit();
-//Creo las Tareas
+void app_main(void)
+{
+	// Inicializo LCD, Switchs y leds
+	LcdItsE0803Init();
+	LedsInit();
+	HcSr04Init(GPIO_3, GPIO_2);
+	SwitchesInit();
+	// Creo las Tareas
 	xTaskCreate(&teclas, "teclas", 512, NULL, 5, NULL);
-    xTaskCreate(&mostrar, "mostrar", 512, NULL, 5, NULL);
-    xTaskCreate(&medicion, "medicion", 512, NULL, 5, NULL);
-
+	xTaskCreate(&mostrar, "mostrar", 512, NULL, 5, NULL);
+	xTaskCreate(&medicion, "medicion", 512, NULL, 5, NULL);
 }
 /*==================[end of file]============================================*/
